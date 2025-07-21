8adb1980298a3d4a48b1d56f3265abac89814c0e
Eric Fahnle <eric.fahnle@gmail.com>
2025-07-21 15:22:57 -0300

    Change formatting

--- hardware.py
+++ hardware.py
@@ -1,17 +1,15 @@
 import RPi.GPIO as GPIO
-#import time
-  
 
 
-def init_gpio(gpio_pin:int) -> GPIO.PWM:
+def init_gpio(gpio_pin: int) -> GPIO.PWM:
     GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-
     GPIO.setup(gpio_pin, GPIO.OUT)
     light_gpio = GPIO.PWM(gpio_pin, 100)
+
     return light_gpio
 
 
-def cleanup():
+def cleanup() -> None:
     GPIO.cleanup()
     print("GPIO cleaned up.")
--- main.py
+++ main.py
@@ -2,34 +2,33 @@ import time
 from config import load_config
 from hardware import init_gpio, cleanup
 
+
 def main():
     config = load_config()
     if config:
         print(f"Configuration loaded successfully: {config}")
-        #print(config)
+        # print(config)
     else:
         print("Failed to load configuration.")
         exit(1)
 
-    light = init_gpio(config.get('gpio_pin', 16))
+    light = init_gpio(config.get("gpio_pin", 16))
     print(light)
-    light.start(100)  
+    light.start(100)
     while True:
         try:
-            for i in range(100,-1,-1):
+            for i in range(100, -1, -1):
                 light.ChangeDutyCycle(i)
-                time.sleep(0.02)           
-        
-            for i in range(100,-1,-1):
+                time.sleep(0.02)
+
+            for i in range(100, -1, -1):
                 light.ChangeDutyCycle(100 - i)
-                time.sleep(0.02)           
+                time.sleep(0.02)
             print("Ciclo completo")
         except KeyboardInterrupt as e:
             light.ChangeDutyCycle(0)
-            print("sali")
             cleanup()
             raise e
-
 
 
 if __name__ == "__main__":
