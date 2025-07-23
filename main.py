f17ddfd226cd7e3d11a70c6186db5a5ed7b209ce
Eric Fahnle <eric.fahnle@gmail.com>
2025-07-23 01:11:43 -0300

    Add cache and http retrieve for sunrisesunset

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from config import load_config
 from hardware import init_gpio, cleanup
+from sunrisesunset import SunriseSunsetWrapper
 
 
 def main():
@@ -11,6 +12,10 @@ def main():
     else:
         print("Failed to load configuration.")
         exit(1)
+
+    sunrise_sunset = SunriseSunsetWrapper(config)
+    data = sunrise_sunset.get_sunrise_sunset()
+    print(f"Days in response: {len(data['results'])}")
 
     light = init_gpio(config.get("gpio_pin", 16))
     print(light)
--- sunrisesunset.py
+++ sunrisesunset.py
@@ -1,2 +1,53 @@
 import requests
+import json
+from pathlib import Path
+from datetime import date, datetime
 
+
+class SunriseSunsetWrapper:
+    def __init__(self, config: dict):
+        self.latitude = config.get("latitude")
+        self.longitude = config.get("longitude")
+        self.api_url = config.get("api_url", "https://api.sunrisesunset.io/json")
+        self.data_file_name = config.get(
+            "data_file_name", "data_cache/sunrise_sunset.json"
+        )
+        # self.gpio_pin = config.get("gpio_pin")
+
+    def get_sunrise_sunset(self) -> dict:
+        if self._cache_exists():
+            print("Using cached data.")
+            return self._get_info_from_from_cache()
+
+        print("Fetching new data from API.")
+        data = self._fetch_sunrise_sunset()
+        self._save_to_cache(data)
+        return data
+
+    def _fetch_sunrise_sunset(self):
+        current_year = datetime.now().year
+        first_day_of_year = date(current_year, 1, 1)
+        last_day_of_year = date(current_year, 12, 31)
+
+        params = {
+            "lat": self.latitude,
+            "lng": self.longitude,
+            "date_start": first_day_of_year,
+            "date_end": last_day_of_year,
+        }
+        
+        response = requests.get(self.api_url, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def _cache_exists(self) -> bool:
+        return Path(self.data_file_name).exists()
+
+    def _get_info_from_from_cache(self) -> dict:
+        with open(self.data_file_name, "r") as file:
+            return json.load(file)
+
+    def _save_to_cache(self, data: dict) -> None:
+        Path(self.data_file_name).parent.mkdir(parents=True, exist_ok=True)
+        with open(self.data_file_name, "w") as file:
+            json.dump(data, file, indent=2)
