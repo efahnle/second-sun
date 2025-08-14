598a9c3058a053652aee0559fa754e3909704908
Eric Fahnle <eric.fahnle@gmail.com>
2025-08-14 19:56:38 -0300

    Add example scheduling in main

--- main.py
+++ main.py
@@ -1,22 +1,59 @@
-import time
+# from hardware import init_gpio, cleanup
 from config import load_config
-from hardware import init_gpio, cleanup
 from sunrisesunset import SunriseSunsetWrapper
+from utils import log
+from apscheduler.schedulers.background import BackgroundScheduler
+import time
+
+
+def display(msg):
+    log(msg)
 
 
 def main():
     config = load_config()
     if config:
-        print(f"Configuration loaded successfully: {config}")
+        log(f"Configuration loaded successfully: {config}")
         # print(config)
     else:
-        print("Failed to load configuration.")
+        log("Failed to load configuration.")
         exit(1)
 
     sunrise_sunset = SunriseSunsetWrapper(config)
     data = sunrise_sunset.get_sunrise_sunset()
-    print(f"Days in response: {len(data['results'])}")
+    log(f"Days in response: {len(data['results'])}")
 
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(
+        display, "interval", seconds=1, args=["every 1 sec"]
+    )  # change to 1 min
+    scheduler.add_job(
+        display,
+        "cron",
+        month=1,
+        day=1,
+        hour=0,
+        minute=0,
+        second=0,
+        args=["every year"],
+    )
+    scheduler.start()
+
+    while True:
+        try:
+            time.sleep(10)
+        except KeyboardInterrupt as e:
+            log(f"Shutdown initiated by KeyboardInterrupt. Setting lamp to 0%")
+            # light.ChangeDutyCycle(0)
+            log("Cleaning up GPIO")
+            # cleanup()
+            log("Shutting down scheduler")
+            scheduler.shutdown(wait=False)
+            log("Exit successfully")
+            raise e
+
+
+"""
     light = init_gpio(config.get("gpio_pin", 16))
     print(light)
     light.start(100)
@@ -34,7 +71,7 @@ def main():
             light.ChangeDutyCycle(0)
             cleanup()
             raise e
-
+"""
 
 if __name__ == "__main__":
     main()
