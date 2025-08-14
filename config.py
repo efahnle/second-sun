2751cdad1b124bbba303801633e6ce55534739bb
Eric Fahnle <eric.fahnle@gmail.com>
2025-08-14 19:56:07 -0300

    Extend exceptions for config parsing

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import json
 
 
-def load_config(file_path='config.json'):
+def load_config(file_path="config.json"):
     """Load configuration from a JSON file."""
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, "r") as file:
             config = json.load(file)
         return config
     except FileNotFoundError:
@@ -12,4 +12,7 @@ def load_config(file_path='config.json'):
         return None
     except json.JSONDecodeError:
         print(f"Error decoding JSON from the configuration file {file_path}.")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred while loading the configuration: {e}")
         return None
