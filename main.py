c213744280e59b3e348b5adfa7bd3b56bc7fef50
Eric Fahnle <eric.fahnle@gmail.com>
2025-08-31 20:24:19 -0300

    Remove comments

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ def main():
     config = load_config()
     if config:
         log(f"Configuration loaded successfully: {config}")
-        # print(config)
     else:
         log("Failed to load configuration.")
         exit(1)
@@ -56,22 +55,7 @@ def main():
 
 """
     light = init_gpio(config.get("gpio_pin", 16))
-    print(light)
-    light.start(100)
-    while True:
-        try:
-            for i in range(100, -1, -1):
-                light.ChangeDutyCycle(i)
-                time.sleep(0.02)
-
-            for i in range(100, -1, -1):
-                light.ChangeDutyCycle(100 - i)
-                time.sleep(0.02)
-            print("Ciclo completo")
-        except KeyboardInterrupt as e:
-            light.ChangeDutyCycle(0)
-            cleanup()
-            raise e
+    light.ChangeDutyCycle(i)
 """
 
 if __name__ == "__main__":
