813912832e9de303c974da5ec736cf8b315cacc2
Eric Fahnle <eric.fahnle@gmail.com>
2025-09-01 11:30:58 -0300

    Add brightness cache

--- hardware.py
+++ hardware.py
@@ -19,4 +19,6 @@ def cleanup() -> None:
 
 def change_light_brightness(lamp: GPIO.PWM, days: dict) -> None:
     brightness = get_light_to_use(days)
-    lamp.ChangeDutyCycle(brightness)
+    if not hasattr(change_light_brightness, 'last_brightness') or change_light_brightness.last_brightness != brightness:
+        lamp.ChangeDutyCycle(brightness)
+        change_light_brightness.last_brightness = brightness
