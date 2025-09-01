11120fc385979ff20b0f5dcbfcb7818ed3c8a722
Eric Fahnle <eric.fahnle@gmail.com>
2025-09-01 09:59:11 -0300

    Fix bug in PWM object

--- hardware.py
+++ hardware.py
@@ -7,6 +7,7 @@ def init_gpio(gpio_pin: int) -> GPIO.PWM:
     GPIO.setwarnings(False)
     GPIO.setup(gpio_pin, GPIO.OUT)
     light_gpio = GPIO.PWM(gpio_pin, 100)
+    light_gpio.start(0)
 
     return light_gpio
 
